7f5689ea0843560f42ca4d7ce5d16f843eede89e
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2024-08-26 23:17:22 -0300

    FEAT: footer finalizado

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -394,18 +394,124 @@
             </div>
         </main>
 
-                    
-            
-            <footer>
-                    
-            </footer>
+
+
+        <footer>
+            <div class="footer">
+                
+
+
+                <div class="cima-footer">
+                        <img src="@/assets/certa.png" alt="" class="logo-footer">
+                        <div class="redes-sociais">
+                            <img src="@/assets/facebook.png" alt="" class="icones-footer">
+                            <img src="@/assets/instagram (1).png" alt="" class="icones-footer">
+                            <img src="@/assets/twitter.png" alt="" class="icones-footer">
+                            <img src="@/assets/whatsapp (1).png" alt="" class="icones-footer">
+                            <img src="@/assets/youtube.png" alt="" class="icones-footer">
+                        </div>
+                </div>
+                <hr style="border: 1px solid black; margin-top: 5vh;">
+                <div class="baixo-footer">
+                    <div class="colunas-footer">
+                    <h2><strong>Categorias</strong></h2>
+                    <ul>
+                        <li>Mais vendidos</li>
+                        <li>produtos</li>
+                        <li>Promoções</li>
+                    </ul>
+                </div>
+                <div class="colunas-footer">
+                    <h2><strong>Empresa</strong></h2>
+                    <ul>
+                        <li>Quem somos?</li>
+                        <li>Por que BeatHub?</li>
+                        <li>Trabalhe conosco</li>
+                    </ul>
+                </div>
+                <div class="colunas-footer">
+                    <h2><strong>Guia de compras</strong></h2>
+                    <ul>
+                        <li>Meios de pagamentos</li>
+                        <li>Compre com pontos</li>
+                        <li>Cartão de crédito</li>
+                    </ul>
+                </div>
+                <div class="colunas-footer">
+                    <h2><strong>Ajuda</strong></h2>
+                    <ul>
+                        <li>Sua conta</li>
+                        <li>Frete e prazo de entrega</li>
+                        <li>Devolução e reembolso</li>
+                    </ul>
+                </div>
+                </div>
+                
+            </div>
+
+        </footer>
 
     </body>
 </template>
 
 <style scoped>
+.icones-footer{
+    width: 2.5vw;
+    margin-top: 5vh;
+    margin-left: 1vw
+}
+.redes-sociais{
+    width: 20vw;
+    height: 10vh;
+}
 
-.container{
+.colunas-footer {
+    height: 10vh;
+    width: 10vw;
+    display: inline-block;
+    margin-top: 5vh;
+    margin-bottom: 15vh;
+
+}
+
+.colunas-footer ul li {
+    list-style: none;
+    color: white;
+    font-family: 'Inter', 'sans serif';
+    font-size: 1vw;
+    text-align: center;
+    margin-top: 1vh;
+    width: 12vw;
+    position: relative;
+    right: 1vw;
+
+}
+
+.colunas-footer h2 {
+    font-size: 1.2vw;
+    font-family: 'Inter', 'sans serif';
+    color: white;
+    text-align: center
+}
+.cima-footer{
+    display: flex;
+    justify-content: space-around;
+    padding-top: 5vh
+}
+.logo-footer{
+    width: 8vw;
+}
+
+.baixo-footer{
+    display: flex;
+    justify-content: space-around;
+}
+.footer {
+    width: 100%;
+    background-color: #9b9a9a;
+}
+
+.container {
     position: relative;
     bottom: 17vh;
 }
@@ -476,7 +582,7 @@
     height: 45vh;
     margin-left: 12px;
     margin-right: 12px;
-    margin-top: 4vh ;
+    margin-top: 4vh;
     box-shadow: -.5px .5px .5px .5px rgb(150, 146, 146);
     margin-bottom: 8vh;
     border-radius: 2vw;
@@ -557,7 +663,7 @@
     height: 64vh;
     width: 80vw;
     transform: translate(-50%, -50%);
-    margin-top: 35vh;
+    margin-top: 33vh;
     display: flex;
     justify-content: space-between;
     position: relative;
@@ -681,8 +787,7 @@ body {
     display: flex;
     display: inline-block;
     position: relative;
-    bottom: 25vh;
-    margin-bottom: -10vw;
+    bottom: 6.5vh;
     box-shadow: 1px 1px 5px 5px rgb(150, 146, 146);
     justify-content: center;
 }
