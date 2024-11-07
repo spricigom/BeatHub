0c18e630ed16784b8c3a566d87c27c1a0012435e
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2024-11-07 00:42:02 -0300

    FEAT: pagina do cartao pronta #59

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,6 +14,7 @@ import { RouterLink } from 'vue-router'
 <RouterLink to="/Membros">Membros </RouterLink>
 <RouterLink to="/CadastroFuncionario">Cadastro Funcionario </RouterLink>
 <RouterLink to="/Login">Login </RouterLink>
+<RouterLink to="/FormaPagamento">FormaPagamento </RouterLink>
 
 
 
