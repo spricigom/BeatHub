615cd80db8b76ed31734d894fdf61c680180225e
spricigom <emilispricigo.ifc@gmail.com>
2024-12-09 16:56:15 -0300

    FEAT: mock centralizadp #96

--- src/mock/produtos/produtos.js
+++ src/mock/produtos/produtos.js
@@ -1,6 +1,6 @@
 
 export const MockProducts = [
-    {
+    {//pagina principal
         id: 1,
         nome: 'Microfone SoundPro SP',
         preco:42.00,
@@ -66,6 +66,389 @@ export const MockProducts = [
         nome: 'Gaita de boca harmônica',
         preco: 17.00 ,
         img: 'gaitaBoca.png', 
+    },
+    {//guitarras
+        id: 11,
+        nome: ' Guitarra Masterbuilt John Frusciante Stratocaster',
+        preco:  42.00 ,
+        img: 'guitarra1.png', 
+    },
+    {
+        id: 12,
+        nome: 'Guitarra Fender® x Hello Kitty® Pink Stratocaster',
+        preco: 17.00 ,
+        img: 'guitarra2.jpg', 
+        
+    },
+    {
+        id: 13,
+        nome: 'Player II Jazzmaster',
+        preco: 'R$ 32,00 por mês',
+        img: 'guitarra3.jpg', 
+
+    },
+    {
+        id: 14,
+        nome: 'Guitarra Jimmy Page Telecaster',
+        preco: 50.00 ,
+        img: 'guitarra4.jpg', 
+        
+    },
+    {
+        id: 15,
+        nome: 'Guitarra Kurt Cobain Jaguar® Left-Han',
+        preco:  30.00 ,
+        img: 'guitarra5.jpg', 
+        
+    },
+    {
+        id: 16,
+        nome: 'Guitarra American Performer Stratocaster® HSS',
+        preco: 35.00 ,
+        img: 'guitarra6.jpg', 
+    },
+    {
+        id: 17,
+        nome: 'Guitarra Eric Clapton Stratocaster®',
+        preco: 69.00 ,
+        img: 'guitarra7.jpg', 
+        
+    },
+    {
+        id: 18,
+        nome: 'Guitarra Jimi Hendrix Stratocaster®',
+        preco:  22.00 ,
+        img: 'guitarra8.jpg', 
+        
+    },
+    {
+        id: 19,
+        nome: 'Guitarra H.E.R. Stratocaster®',
+        preco:  40.00,
+        img: 'guitarra9.jpg', 
+        
+    },
+    {
+        id: 20,
+        nome: 'Guitarra Tom Morello Stratocaster®',
+        preco: 17.00 ,
+        img: 'guitarra10.jpg', 
+    },
+     
+    {//baixos
+        id: 21,
+        nome: 'Baixo American Professional II Precision Bass®',
+        preco: 17.00 ,
+        img: 'baixo1.jpg', 
+    },
+    {
+        id: 22,
+        nome: 'Baixo American Ultra II Meteora® Bass',
+        preco: 42.00 ,
+        img: 'baixo2.jpg', 
+        
+    },
+    {
+        id: 23,
+        nome: 'Baixo Duff McKagan Deluxe Precision Bass®',
+        preco: 32.00 ,
+        img: 'baixo3.jpg', 
+
+    },
+    {
+        id: 24,
+        nome: 'Baixo Limited Edition Mark Hoppus Jaguar® Bass',
+        preco: 24.00 ,
+        img: 'baixo4.jpg', 
+        
+    },
+    {
+        id: 25,
+        nome: 'Baixo Player Plus Active Jazz Bass®',
+        preco:  30.00,
+        img: 'baixo5.jpg', 
+        
+    },
+    {
+        id: 26,
+        nome: 'Baixo Vintera® II 50s Precision Bass®',
+        preco: 35.00 ,
+        img: 'baixo.jpg', 
+    },
+    {
+        id: 27,
+        nome: 'Baixo Troy Sanders Precision Bass®',
+        preco: 69.00 ,
+        img: 'baixo7.jpg', 
+        
+    },
+    {
+        id: 28,
+        nome: 'Baixo American Performer Mustang® Bass',
+        preco:  50.00,
+        img: 'baixo8.jpg', 
+        
+    },
+    {
+        id: 29,
+        nome: 'Baixo Classic Vibe 70s Jazz Bass® V',
+        preco:  40.00,
+        img: 'baixo9.jpg', 
+        
+    },
+    {
+        id: 30,
+        nome: 'Baixo Tony Franklin Fretless Precision Bass®',
+        preco: 17.00 ,
+        img: 'baixo10', 
+    },    
+    {//pedais
+        id:31,
+        nome:'BOSS DS-1W | Pedal de distorção Waza Craft',
+        preco: 42.00,
+        img: 'pedal1.png',
+    },
+    {
+        id: 32,
+        nome:'BOSS MT-2W | Pedal de Distorção Metal Zone Waza Craft',
+        preco: 17.00,
+        img: 'pedal2.webp',
+    },
+    {
+        id: 33,
+        nome:'BOSS FZ-1W | Pedal de Fuzz Waza Craft',
+        preco: 32.00,
+        img: 'pedal3.png',
+    },
+    {
+        id: 34,
+        nome:'BOSS CE-2W | Pedal de Chorus Waza Craft',
+        preco: 69.00,
+        img: 'pedal4.webp',
+    },
+    {
+        id: 35,
+        nome:'BOSS VB-2W | Pedal de modulação vibrato Waza Craft',
+        preco: 22.00,
+        img: 'pedal5.webp',
+    },
+    {
+        id: 36,
+        nome:'Pedal de Reverb Digital BOSS RV-6 Reverb',
+        preco: 50.00,
+        img: 'pedal6.webp',
+    },
+    {
+        id: 37,
+        nome:'Pedal de Modulação BOSS TR-2 Tremolo',
+        preco: 40.00,
+        img: 'pedal7.webp',
+    },
+    {
+        id: 38,
+        nome:'Pedal de Overdrive/ Distortion BOSS JB-2 Angry Driver',
+        preco: 35.00,
+        img: 'pedal8.webp',
+    },
+    {
+        id: 39,
+        nome:'BOSS NS-2 | Pedal Supressor de Ruído',
+        preco: 17.00,
+        img: 'pedal9.png',
+    },
+    {
+        id: 40,
+        nome:'Pedal Harmonizador e Pitch Shifter BOSS PS-6 Harmonist',
+        preco: 69.00,
+        img: 'pedal10.webp', 
+    },
+    {//teclados
+        id: 41,
+        nome: 'Teclado  1PSR',
+        preco: 92.00,
+        img: 'piano1.jpg', 
+    },
+    {
+        id: 42,
+        nome: 'Teclado Cst 3000',
+        preco: 77.00,
+        img: 'piano2.jpg', 
+    },
+    {
+        id: 43,
+        nome: 'Teclado 3Pss',
+        preco: 72.00 ,
+        img: 'piano3.jpg', 
+    },
+    {
+        id: 44,
+        nome: 'Teclado 4Pss',
+        preco: 84.00,
+        img: 'piano4.jpg', 
+    },
+    {
+        id: 45,
+        nome: 'Teclado 4PSR',
+        preco: 31.00,
+        img: 'piano5.avif',         
+    },
+    {
+        id: 46,
+        nome: 'Teclado MXT',
+        preco: 38.00 ,
+        img: 'piano6.avif', 
+    },
+    {
+        id: 47,
+        nome: 'Teclado 4EZ',
+        preco: 69.00,
+        img: 'piano7.jpg', 
+    },
+    {
+        id: 48,
+        nome: 'Teclado 4PSR',
+        preco: 20.00,
+        img: 'piano8.jpg', 
+    },
+    {
+        id: 49,
+        nome: 'Teclado MXT 128',
+        preco: 44.00 ,
+        img: 'piano10.avif', 
+        
+    },
+    {
+        id: 50,
+        nome: 'Teclado 2PSR',
+        preco: 50.00,
+        img: 'piano11.jpg', 
+    },
+    {//violao
+        id: 51,
+        nome: 'Violao Yamaha C8OII',
+        preco: 42.00,
+        img: 'violao1.webp',
+    },
+    {
+        id: 52,
+        nome: 'Violao Yamaha C40M',
+        preco: 17.00,
+        img: 'violao2.webp',
+    },
+    {
+        id: 53,
+        nome: 'Violao Yamaha F-130',
+        preco: 69.00,
+        img: 'violao3.webp',
+    },
+    {
+        id: 54,
+        nome: 'Violao Phoenix Nylon NL4BK',
+        preco: 24.00,
+        img: 'violao4.webp',
+    },
+    {
+        id: 55,
+        nome: 'Violao Infantil Vogga VCA85',
+        preco: 40.00,
+        img: 'violao5.webp',
+    },
+    {
+        id: 56,
+        nome: 'Violao Giannini Classico',
+        preco:  69.00,
+        img: 'violao6.jpg',
+    },
+    {
+        id: 57,
+        nome: 'Violao Giannini DG1',
+        preco:  20.00,
+        img: 'violao7.webp',
+    },
+    {
+        id: 58,
+        nome: 'Violao Fender ACB ',
+        preco:  44.00,
+        img: 'violao8.webp',
+    },
+    {
+        id: 59,
+        nome: 'Violao Memphis Ac39R',
+        preco:  69.00,
+        img: 'violao9.webp',
+    },
+    {
+        id:60,
+        nome: 'Violao Modena Nylon',
+        preco:  35.00,
+        img: 'violao10.webp',
+    },
+    {//bateria
+        id: 61,
+        nome: 'Bateria Gretsch',
+        preco:  92.00,
+        img: 'bateria1.webp', 
+    },
+    {
+        id: 62,
+        nome: 'Bateria Recording Custom',
+        preco:  77.00,
+        img: 'bateria2.webp', 
+        
+    },
+    {
+        id: 63,
+        nome: 'Bateria American Musical',
+        preco:  72.00,
+        img: 'bateria33.jpeg', 
+
+    },
+    {
+        id: 64,
+        nome: 'Bateria D one Grestch',
+        preco:  84.00,
+        img: 'bateria4.webp', 
+        
+    },
+    {
+        id: 65,
+        nome: 'Bateria Well Drums',
+        preco:  31.00,
+        img: 'bateria5.webp', 
+        
+    },
+    {
+        id: 66,
+        nome: 'Bateria Fusion',
+        preco:  38.00,
+        img: 'bateria6.webp', 
+    },
+    {
+        id: 67,
+        nome: 'Bateria AudioTek',
+        preco:  69.00,
+        img: 'bateria7.webp', 
+        
+    },
+    {
+        id: 68,
+        nome: 'Bateria Tama Stagestar',
+        preco:  20.00,
+        img: 'bateria8.jpeg', 
+        
+    },
+    {
+        id: 69,
+        nome: 'Bateria Bumbo 20',
+        preco:  44.00,
+        img: 'bateria9.jpeg', 
+        
+    },
+    {
+        id: 70,
+        nome: 'Bateria Power ROckers',
+        preco:  50.00,
+        img: 'bateria10.webp', 
     }
 ]
 export const favoritos = MockProducts.filter(p => p.favoritado)
