3ffe7221eba2983dbda1cb19e6de6f8571557104
spricigom <emilispricigo.ifc@gmail.com>
2024-12-10 15:17:29 -0300

    FEAT: descricao dos produtos da pagina principal #96

--- src/mock/produtos/produtos.js
+++ src/mock/produtos/produtos.js
@@ -4,6 +4,7 @@ export const MockProducts = [
         id: 1,
         nome: 'Microfone SoundPro SP',
         preco:42.00,
+        descricao: 'O Microfone SoundPro SP é um modelo acessível e de boa qualidade, ideal para quem busca um microfone confiável para gravações de voz, podcasts, transmissões ao vivo e outras aplicações. Geralmente, apresenta design robusto, captação unidirecional (cardioide) e oferece uma boa relação custo-benefício. É fácil de usar, com conexões XLR ou USB, sendo adequado tanto para iniciantes quanto para usuários mais experientes. Embora não tenha os recursos avançados de modelos profissionais, oferece uma performance sólida para quem precisa de um microfone versátil e de baixo custo.',
         img: 'microfone.png', 
         
     },
@@ -11,12 +12,14 @@ export const MockProducts = [
         id: 2,
         nome: 'Pedal Over Drive',
         preco :17.00 ,
+        descricao: 'Um pedal Overdrive é um efeito de guitarra utilizado para criar distorção suave ou saturação no som, simulando o comportamento de amplificadores valvulados quando sobrecarregados. Ele é popular em estilos como rock, blues e hard rock, pois adiciona "quente" e "grit" ao som, dando uma sonoridade mais rica e encorpada. Ao ativar o pedal, o sinal da guitarra é distorcido, geralmente de forma mais suave e controlada do que um pedal de distorção, preservando características tonais da guitarra. Muitos pedais overdrive permitem ajustes de ganho, tom e nível, permitindo ao músico controlar a intensidade e o caráter da distorção. O overdrive é bastante apreciado por sua capacidade de produzir um som "orgânico", muitas vezes com a sensação de que o amplificador está "forçando" suas válvulas, o que resulta em uma saturação natural e dinâmica, que responde ao toque do guitarrista.',
         img: 'pedal-OverDrive.png', 
     },
     {
         id: 3,
         nome: 'Cubo Amplificador',
         preco :32.00 ,
+        descricao: 'O cubo amplificador é um amplificador compacto e portátil, ideal para guitarras elétricas, oferecendo uma solução prática para músicos que precisam de um equipamento leve e fácil de transportar. Com potências que variam de 10W a 100W, esses amplificadores são comuns para ensaios, apresentações pequenas ou gravações caseiras. Além disso, muitos modelos vêm com controles de volume, ganho, tonalidade e efeitos integrados como reverb e overdrive, oferecendo versatilidade para diferentes estilos musicais.',
         img: 'amplificador.png', 
 
     },
@@ -24,6 +27,7 @@ export const MockProducts = [
         id: 4,
         nome: 'Ukulele Classic',
         preco :24.00 ,
+        descricao:'O Ukulele Classic é um instrumento pequeno e tradicional de quatro cordas, com corpo de madeira, geralmente afinado em C (sol). Conhecido por seu som alegre e vibrante, é popular entre iniciantes e músicos que buscam um som suave e melódico. Usado em estilos como havaiiano e folk, o Ukulele Classic é fácil de tocar, acessível e ideal para quem procura um instrumento descontraído e de timbre único.',
         img: 'ukulele2.png', 
         
     },
@@ -31,6 +35,7 @@ export const MockProducts = [
         id: 5,
         nome: 'Pandeiro',
         preco:32.00 ,
+        descricao:'O pandeiro é um instrumento de percussão tradicionalmente utilizado na música brasileira, especialmente em gêneros como samba, choro e MPB. Ele é composto por uma tampa circular (geralmente de madeira) e tambores com platinelas (pequenos címbalos metálicos) nas bordas, que produzem o característico som chocalhante quando agitado ou tocado com as mãos. O pandeiro pode ser tocado de várias maneiras, incluindo batidas com os dedos, palmas e batidas de mão aberta, oferecendo uma grande variedade de timbres e ritmos. Além de ser um dos principais instrumentos na música popular brasileira, é também utilizado em outros estilos de música latina e mundial, pela sua versatilidade e presença marcante.',
         img: 'pandeiro.png', 
         
     },
@@ -38,33 +43,38 @@ export const MockProducts = [
         id: 6,
         nome: 'Fender x Hello Kitty Pink Stratocaster',
         preco: 35.00 ,
+        descricao: 'A Fender Hello Kitty Pink Stratocaster é uma guitarra elétrica que combina o design clássico da famosa Stratocaster com um toque único e lúdico inspirado na personagem Hello Kitty, da Sanrio. Lançada como um modelo especial, ela apresenta um acabamento rosa brilhante, com detalhes exclusivos da Hello Kitty, como adesivos e uma imagem da personagem no headstock (a parte superior do braço). Além de seu visual atraente, o modelo mantém a qualidade e o som característicos da Fender, com três captadores single-coil, que proporcionam um som claro e brilhante, ideal para uma ampla gama de estilos musicais, como pop, rock e música alternativa. A Fender Hello Kitty Pink Stratocaster é uma guitarra voltada para fãs da marca, colecionadores e músicos que buscam um instrumento com um design diferenciado, mas com o desempenho e a confiabilidade da Fender.',
         img: 'guitarra2.jpg', 
     },
     {
         id: 7,
         nome: 'Trompete Werill Master',
         preco :69.00 ,
+        descricao:'O Trompete Werill Master é um modelo de trompete de alta qualidade, projetado para músicos que buscam um instrumento com excelente desempenho tanto em performances profissionais quanto em ambientes educacionais. A marca Werill é conhecida por produzir trompetes com boa projeção sonora, afinação precisa e materiais duráveis, com um acabamento refinado. O modelo Master geralmente apresenta tubos em latão de alta qualidade, válvulas bem construídas e uma resposta rápida, o que facilita a execução de passagens rápidas e notas agudas. É um trompete versátil, adequado para diversos estilos musicais, como jazz, música clássica e até fanfarras. O Werill Master é uma boa escolha para músicos intermediários e avançados que procuram um trompete com características profissionais a um preço acessível.',
         img: 'trompete.png', 
         
     },
     {
         id: 8,
         nome: 'Bateria Acústica',
-        preco: 287.00 ,
+        preco: 200.00 ,
+        descricao:'A bateria acústica é um conjunto de instrumentos de percussão composto por tambores (como o bumbo, caixa, tonéis), pratos e acessórios (como hi-hat, crash e ride), usados em diversos estilos musicais. O som é gerado pela vibração das peles tensionadas dos tambores e pelo impacto das baquetas nos pratos. A bateria acústica é apreciada por sua sonoridade rica e dinâmica, sendo essencial em performances ao vivo, gravações e em estúdios. Ela oferece ao baterista total controle sobre o timbre e a intensidade do som.',
         img: 'bateria.png', 
         
     },
     {
         id: 9,
-        nome: 'Prato Hi Hat',
+        nome: 'Pratos Fanfarra 18" em Latão',
         preco: 42.00 ,
-        img: 'prato.png', 
+        descricao: 'Os Pratos de Fanfarra 18" em Latão são instrumentos de percussão projetados especialmente para fanfarras e bandas marciais. Com um diâmetro de 18 polegadas, esses pratos oferecem um som claro e projetado para cortes nítidos, essenciais para marcar o ritmo em desfiles e performances ao vivo. O latão é um material comum na fabricação desses pratos devido à sua durabilidade, resistência e boa projeção sonora. Eles produzem um som brilhante e intenso, ideal para situações de grande exposição sonora, como em paradas, cerimônias e outros eventos ao ar livre. Esses pratos geralmente são mais leves do que os usados em baterias tradicionais, facilitando o manuseio durante movimentos rápidos e dinâmicos.',
+        img: 'prato.webp', 
         
     },
     {
         id: 10,
         nome: 'Gaita de boca harmônica',
         preco: 17.00 ,
+        descricao: 'A gaita de boca harmônica, também conhecida simplesmente como gaita, é um pequeno instrumento de sopro composto por uma série de palhetas de metal montadas em uma estrutura de madeira ou plástico. Ao soprar ou aspirar através dos buracos na gaita, as palhetas vibram, produzindo sons de diferentes notas. As gaitas de boca são muito populares em estilos musicais como blues, rock, folk e country, sendo apreciadas por sua portabilidade e facilidade de uso. Existem diferentes tipos de harmônicas, como diatônicas (para tocar em uma escala específica) e ** cromáticas** (que oferecem uma gama maior de notas, incluindo sustenidos e bemóis), permitindo uma grande versatilidade musical.',
         img: 'gaitaBoca.png', 
     },
     {//guitarras
