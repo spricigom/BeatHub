e8946bb137e6a2ca18aab666645e1c79d07b87fe
SaraKozoski <sarakozoski@gmail.com>
2024-12-12 09:39:16 -0300

    FEAT: Banco de dados/ produto  paginas #115

--- src/utils/getProductImage.js
+++ src/utils/getProductImage.js
@@ -10,3 +10,27 @@ export function getImage(productIndex) {
     console.log(url)
     return url
 }
+export function getImage2(productIndex) {
+    console.log(productIndex)
+    const product = produtoStore.produtos.find(p => p.id === productIndex)
+    console.log(product)
+    const url = new URL(`../assets/${product?.img2}`, import.meta.url).href
+    console.log(url)
+    return url
+}
+export function getImage3(productIndex) {
+    console.log(productIndex)
+    const product = produtoStore.produtos.find(p => p.id === productIndex)
+    console.log(product)
+    const url = new URL(`../assets/${product?.img3}`, import.meta.url).href
+    console.log(url)
+    return url
+}
+export function getImage4(productIndex) {
+    console.log(productIndex)
+    const product = produtoStore.produtos.find(p => p.id === productIndex)
+    console.log(product)
+    const url = new URL(`../assets/${product?.img4}`, import.meta.url).href
+    console.log(url)
+    return url
+}
